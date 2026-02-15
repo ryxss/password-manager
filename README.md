a3adbb2f6cafff1afdc627d2c6d6c967deef7d74
ryxss <prathamahkr@gmail.com>
2026-02-15 14:16:56 -0600

    Optimized function calls

---
+++ README.md
@@ -0,0 +1,4 @@
+# Secure Terminal based Password Manager
+password-manager generates secure passwords and stores them locally by utlizing a simple numbers-based encrypting technique.
+Made as a solution to most browsers storing and exporting passwords in plain text. 
+Existing passwords may also be added. csv format is chosen for simplicity, and it can be updated to JSON if desired.
--- Scripts/passwordManager.py
+++ Scripts/passwordManager.py
@@ -6,7 +6,6 @@ parentDir = os.path.dirname(__file__)
 storageDir = os.path.join(parentDir, "../.user/")
 if not os.path.exists(storageDir):
     os.makedirs(storageDir)
-
 userFile = os.path.join(storageDir, "loggedUsers.csv")
 
 def __set_session(userName=None, passHash=None, loggedIn=1):
@@ -16,6 +15,7 @@ def __set_session(userName=None, passHash=None, loggedIn=1):
         __session['userLoggedIn'] = 1
         __session['dataFile'] = os.path.join(storageDir, __session['safeUserName']+".csv") 
         __session['hash'] = passHash
+
     else:
         __session['plainUserName'] = None
         __session['safeUserName'] = None
@@ -35,112 +35,101 @@ def checkUserNameAvail(userName):
     try:
         if userName == "" : return -1
         safeUserName = encrypt.encrypt(userName, userName)
-        loggedUsers = open(userFile, newline='')
-        checkProfiles = csv.reader(loggedUsers, delimiter = ' ', quotechar='|')
-        for row in checkProfiles:
-            if row[0] == safeUserName:
-                loggedUsers.close()
-                return 0
-        loggedUsers.close()
+        with open(userFile, newline='') as loggedUsers:
+            checkProfiles = csv.reader(loggedUsers, delimiter = ' ', quotechar='|')
+            for row in checkProfiles:
+                if row[0] == safeUserName:
+                    return 0
         return 1
     except FileNotFoundError:
         return 2
 
 def handleUserAction(returning=1):
+    def processPasswordSave():
+        saveStatus = input('Would you like to save the password?(Y/N) ').lower()
+        if saveStatus == 'n':
+            print("Password was not saved.")   
+        elif saveStatus == 'y':
+            passkeyword = input('Determine a keyword for the password that will help you recognize it later: ') or "deafault"
+            if savePassword(password, passkeyword) == 1:
+                print(f'Password saved with the keyword {passkeyword}!')
+            else:
+                print("Saving failed. Try again.")      
+        else:
+            raise InputError    
+
     try: 
-        if returning == 1:
-            prompt = "Create a new password(1), add an existing password to profile(2) or access existing passwords(3) or exit(9): "
-        else: 
-            prompt = "Create a new password(1), add an existing password to profile(2) or exit(9): "
-        
-        userActionMode = input(prompt)
+        prompt = "\nCreate a new password(1), add an existing password to profile(2) or access existing passwords(3)," \
+        "export passwords(4), help, or exit(9): "
+        if returning != 1:
+            print(prompt)
+        userActionMode = input("Action: ")
         if userActionMode == '3':
             displaySavedPasswords()
-            return
-        elif userActionMode == '2':
+        elif userActionMode == '2':    
             password = input('Enter the password: ')
-        elif userActionMode == '1':
+            processPasswordSave()
+        elif userActionMode == '1':    
             password = createPassword()
             print("Password created!")
             print(password)
-            saveStatus = input('Would you like to save the password?(Y/N) ').lower()
-            if saveStatus == 'n':
-                print("Password was not saved.")
-                return
+            processPasswordSave()
         elif userActionMode == "help":
-            userHelp()
+            print(prompt)
         elif userActionMode == '9':
-            return logout()
+            logout()
         else:
-            print("Please pick one of the given options or enter help.")
-            return   
-        passkeyword = input('Determine a keyword for the password that will help you recognize it later: ')
-        if savePassword(password, passkeyword) == 1:
-            print(f'Password saved with the keyword {passkeyword}!')
-            return
-        else:
-            print("Saving failed. Try again.")
-            return
+            raise InputError
+        
     except (InputError, ValueError):
-        print('Please choose one of the available options!')
+        print('Please choose one of the available options or enter help!')
 
 def checkUserProfile(inputName, inputPass):
     safeName = encrypt.encrypt(inputName, inputName)
     try:
-        loggedUsers = open(userFile, newline='')
-        checkProfiles = csv.reader(loggedUsers, delimiter = ' ', quotechar='|')
-        encodedPass = inputPass.encode(encoding='ascii')
-        passHash = hashlib.blake2b(encodedPass).hexdigest()
-        for creds in checkProfiles:
-            if creds[0] == safeName and creds[1] == passHash:
-                __set_session(inputName, passHash)
-                loggedUsers.close()
-                return 1
-            elif (creds[0] != safeName and creds[1] == passHash) or (creds[0] == safeName and creds[1] != passHash):
-                loggedUsers.close()
-                return 2
-        loggedUsers.close()    
+        with open(userFile, newline='') as loggedUsers:
+            checkProfiles = csv.reader(loggedUsers, delimiter = ' ', quotechar='|')
+            encodedPass = str(inputPass).encode(encoding='ascii')
+            passHash = hashlib.blake2b(encodedPass).hexdigest()
+            for creds in checkProfiles:
+                if creds[0] == safeName and creds[1] == passHash:
+                    __set_session(inputName, passHash)
+                    return 1
+                elif (creds[0] != safeName and creds[1] == passHash) or (creds[0] == safeName and creds[1] != passHash):
+                    return 2  
         return 0               
     except FileNotFoundError:
         return 3
 
-#This is perfect dont change for the love of love       
+#This is perfect dont change   
 def createPassword():
     specialSymbols = '@$%&*_-#'
     tries = 0
-    while tries < 3:
+    while tries < 2:
         try:
-            codeLen = int(input('What length would you like your password to be? '))
-            if codeLen > 10000:
-                print('The length of the password can not exceed 10_000.')
-                raise ValueError
-            tries = 0
-            break
+            codeLen = input('How many characters? (default = 16): ') or 16
+            if 0 < int(codeLen) <= 10000:
+                tries = 0
+                break
+            else:
+                print('Password length can not be negative or exceed 10_000 (Recommended: 16).')
+                tries += 1
         except ValueError:
-            print('Please enter a valid value.(Recommended: 16)')
-            tries += 1
-        if tries == 3:
-            print('Try again later.')
-            return 0
-    while tries < 3:
-        try:
-            useSym = input('Would you like to use special symbols in the password?(Y/N) ')
-            tries = 0
-            break
-        except ValueError:
-            print('Please choose one of the given options.')
-            tries += 1
-        if tries == 3:
-            print('Try again later.')
-            return
+            print("Length is a positive integer > 0. Using 16.")
+            codeLen = 16
+    if tries == 3:
+        print('Try again later.')
+        return 0
+    
+    useSym = input('Would you like to use special symbols in the password?(Y/n): ') or 'y'
+            
     useSym = useSym.lower()
-    if useSym == 'y':
-        x = ''.join(secrets.choice(string.ascii_letters + string.digits + str(specialSymbols)) for i in range(codeLen))
-    elif useSym == 'n':
+    
+    if useSym == 'n':
         x = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(codeLen))
     else:
-        raise InputError(useSym, 'Invalid input')
-    return x
+        x = ''.join(secrets.choice(string.ascii_letters + string.digits + str(specialSymbols)) for i in range(codeLen))
+    return str(x)
 
 def savePassword(password, passkeyword):
     try:
@@ -166,19 +155,16 @@ def displaySavedPasswords():
         print(f"\nDisplaying passwords saved for {__session['plainUserName']}")
         for row in checkSavedPasswords:
             if row[0] == __session['safeUserName']:
-                decryptedPassword = encrypt.decrypt(row[2], __session["hash"])
+                decryptedPassword = encrypt.decrypt(str(row[2]), __session["hash"])
                 #if pw starts with a b', it will be stored as b'b'.... so this is necessary for clean output
                 decryptedPassword = decryptedPassword[2:-1] if decryptedPassword.startswith("b'") else decryptedPassword
-                decryptedKeyword = encrypt.decrypt(row[1], __session['hash'])
+                decryptedKeyword = encrypt.decrypt(str(row[1]), __session["hash"])
                 print(f'Keyword: {decryptedKeyword}  Password: {decryptedPassword}')
-                x = 1
-            else:
-                x = 2
-        if x == 2:
+        else:
             print(f'\nNo passwords found for {__session["plainUserName"]}.')
         passwordFile.close()
     except FileNotFoundError:
-        print(f'\nNo passwords found for {__session["plainUserName"]}.')
+        print(f'\nNo password file found for {__session["plainUserName"]}. Was it deleted?')
 
 def exportPasswords():
     if __session["userLoggedIn"] == 0:
@@ -240,17 +226,16 @@ def signUp():
     elif userNameGood == 0:
         print("Looks like you've already been here. Username taken. Try login.")
     elif userNameGood == 1 or userNameGood == 2:
-        print('Before you proceed, note the password you set. You can not recover saved passwords without your master password.')
+        print('Before you proceed, note the password you set. You can not recover saved passwords without your master password.\n')
         userPassword = getpass.getpass(prompt='Enter a password: ')    
         userPassConf= getpass.getpass(prompt='Confirm master password: ')
         if userPassword != userPassConf:
             print("Passwords don't match. Restart.")
-            return -1
-        userCreated = logUserProfile(userName, userPassword)
-        if userCreated == 1:
-            print(f"User Created Successfully. Logging in as {userName}.")
-            __set_session(userName)
-            handleUserAction(0)
+        else:
+            userCreated = logUserProfile(userName, userPassword)
+            if userCreated == 1:
+                print(f"User Created Successfully. Logging in as {userName}.\n")
+                handleUserAction(0)
 
 def userHelp():
     commandActions = {'login' : 'Log in to your existing account.', 
@@ -268,28 +253,29 @@ def unregUserPassword():
     tempPassword = createPassword()
     print(tempPassword)
 
-print('Welcome to password-manager. Enter --help or --commands for help.')
-
 def inputStream():
     print()
     userInput = str(input("$ ")).lower()
-    if userInput == 'login':
+    if userInput == "login":
         login()
-    elif userInput == 'create-account' or userInput == 'createacc':
+    elif userInput == "create-account" or userInput == "createacc" or userInput == "signup":
         signUp()
-    elif userInput == 'logout':
+    elif userInput == "logout":
         logout()
-    elif userInput == 'commands' or userInput == 'help':
+    elif userInput == "commands" or userInput == "help":
         userHelp()
-    elif userInput == 'create-password':
+    elif userInput == "create-password":
         unregUserPassword()
-    elif userInput == 'exit' or userInput == 'quit' or userInput=='9':
+    elif userInput == "exit" or userInput == "quit" or userInput=='9':
         exit()
     elif userInput == "export":
         exportPasswords()
     else:
-        print('Invalid command. Enter help/commands to get a complete list of commands.')
+        print("Invalid command. Enter help/commands to get a complete list of commands.")
 
+#PROGRAM ENTERS HERE
+
+print('Welcome to password-manager. Enter help or commands for help.')
 while True:
     if __session['userLoggedIn'] == 1:
         handleUserAction()
